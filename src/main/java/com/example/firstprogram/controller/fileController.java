71c4370070c7799c7bce52f5844b00679d7ffd41
anhyeongkwan <bombom841@naver.com>
2023-12-10 22:47:06 +0900

    - qna editor 이미지 업로드

--- src/main/java/com/example/firstprogram/controller/fileController.java
+++ src/main/java/com/example/firstprogram/controller/fileController.java
@@ -1,18 +1,35 @@
 package com.example.firstprogram.controller;
 
+import com.example.firstprogram.util.Util;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
+@Log4j2
+@RequiredArgsConstructor
 public class fileController {
 
+    private final Util util;
+
+    // consumes = MediaType.MULTIPART_FORM_DATA_VALUE : swagger 상에서 파일 전송 가능
     @ResponseBody
     @PostMapping(value = "/editorFileUpload", produces = "application/json;charset=utf-8")
-    public String editorFileUpload(HttpServletRequest request, MultipartFile[] multipartFiles){
-        return "";
+    public String editorFileUpload(HttpServletRequest request, @RequestParam("files") MultipartFile[] files){
+
+        String savePath = "/editor/qna/";
+
+        JSONObject fileJson = util.fileUpload(request, files, savePath);
+
+        String filepath = (String) fileJson.get("filepath");
+        return filepath;
     }
 
 }
---
+++ src/main/java/com/example/firstprogram/util/Util.java
@@ -0,0 +1,82 @@
+package com.example.firstprogram.util;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.log4j.Log4j2;
+import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Component
+@Log4j2
+@PropertySource("classpath:application.properties")
+public class Util {
+
+    @Value("${upload.path}")
+    private String defaultPath;
+
+    /*
+    * file upload
+    * */
+    public JSONObject fileUpload(HttpServletRequest request, MultipartFile[] multipartFile, String filePath){
+
+        JSONObject jsonObject = new JSONObject();
+        log.info("defaultPath : " + defaultPath);
+        if(multipartFile.length > 0){
+            String savePath = defaultPath + filePath;
+            log.info("savepath : " + savePath);
+            for (MultipartFile file : multipartFile) {
+                String filename = file.getOriginalFilename();
+                log.info("filename : " + filename);
+
+                try {
+                    FileOutputStream fos = new FileOutputStream(savePath + filename);
+                    BufferedOutputStream bos = new BufferedOutputStream(fos);
+                    byte[] bytes = file.getBytes();
+
+                    bos.write(bytes);
+                    bos.close();
+
+                    jsonObject.put("filename", filename);
+                    jsonObject.put("filepath", savePath + filename);
+                } catch (FileNotFoundException e) {
+                    throw new RuntimeException(e);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+        }
+
+        return jsonObject;
+    }
+
+    /*public String fileReName(String savePath, String filename) {
+
+        String filepath = "";
+
+        String onlyFilename = filename.substring(0, filename.lastIndexOf("."));
+        String extention = filename.substring(filename.lastIndexOf("."));
+
+        log.info("onlyFilename : " + onlyFilename);
+        log.info("extention : " + extention);
+        int cnt = 0;
+        while (true) {
+            cnt++;
+            filepath = savePath + onlyFilename + "(" + cnt + ")" + extention;
+
+            File fileChk = new File(savePath + filename);
+            if (fileChk.exists()) {
+                filepath = savePath + onlyFilename + "(" + 1 + ")" + extention;
+            }
+        }
+        return "";
+    }*/
+
+}
